c09dbb3ee3bd699a2ca5755274852449badc48ca
romain-mottet <60892372+romain-mottet@users.noreply.github.com>
2020-03-25 08:49:09 +0100

    code pour heberger en local
    #code pour heberger en local la page via windowsshell 
    #Commun à tout le monde, ne pas toucher

---
+++ serv.py
@@ -0,0 +1,19 @@
+#code pour heberger en local la page via windowsshell 
+#Commun à tout le monde, ne pas toucher
+
+
+#coding: utf-8
+import http.server
+
+port = 80
+address = ("", port)
+
+server = http.server.HTTPServer
+
+handler = http.server.CGIHTTPRequestHandler
+handler.cgi_directories = ["/"]
+
+httpd = server(address, handler)
+
+print (f"sereveur démarré sur le PORT {port}")
+httpd.serve_forever()
